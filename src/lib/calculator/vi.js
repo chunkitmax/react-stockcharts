8a0be1e0eb9e1d506fa232ef68560a39101abd6d
chunkitmax <chunkitmax1125@gmail.com>
2019-09-13 01:06:15 +0800

    minor format problem

--- src/lib/calculator/vi.js
+++ src/lib/calculator/vi.js
@@ -2,7 +2,7 @@
 
 import { sum } from "d3-array";
 
-import { isDefined, last, slidingWindow, path } from "../utils";
+import { slidingWindow } from "../utils";
 import { VI as defaultOptions } from "./defaultOptionsForComputation";
 
 export default function() {
@@ -16,34 +16,34 @@ export default function() {
 			.windowSize(windowSize)
 			.accumulator((values) => {
 
-        const sumTrueRange = sum(values, d => d.trueRange)
-        const sumVmUp = sum(values, d => d.vmUp)
-        const sumVmDown = sum(values, d => d.vmDown)
+				const sumTrueRange = sum(values, d => d.trueRange);
+				const sumVmUp = sum(values, d => d.vmUp);
+				const sumVmDown = sum(values, d => d.vmDown);
 
 				return {
-          vmPlus: sumVmUp/sumTrueRange,
-          vmMinus: sumVmDown/sumTrueRange,
-        };
+					vmPlus: sumVmUp / sumTrueRange,
+					vmMinus: sumVmDown / sumTrueRange,
+				};
 			});
 
-    const setupCalculator = slidingWindow()
-    .windowSize(2)
-    .undefinedValue(() => [
-      { close: 0, high: 0, low: 0 },
-      { close: 0, high: 0, low: 0 }
-    ]).accumulator(tuple => {
-      const prev = tuple[0];
-      const cur = tuple[1];
-      return {
-        trueRange: Math.max(
-          cur.high-cur.low,
-          Math.abs(cur.low-prev.close),
-          Math.abs(cur.high-prev.close)
-        ),
-        vmUp: Math.abs(cur.high-prev.low),
-        vmDown: Math.abs(cur.low-prev.high)
-      }
-    })
+		const setupCalculator = slidingWindow()
+			.windowSize(2)
+			.undefinedValue(() => [
+				{ close: 0, high: 0, low: 0 },
+				{ close: 0, high: 0, low: 0 }
+			]).accumulator(tuple => {
+				const prev = tuple[0];
+				const cur = tuple[1];
+				return {
+					trueRange: Math.max(
+						cur.high - cur.low,
+						Math.abs(cur.low - prev.close),
+						Math.abs(cur.high - prev.close)
+					),
+					vmUp: Math.abs(cur.high - prev.low),
+					vmDown: Math.abs(cur.low - prev.high)
+				};
+			});
 
 		const viData = viAlgorithm(setupCalculator(data));
 
